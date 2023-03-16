a7eb68a63d039a70508f6d5910eac26698f0c0ab
AngelKode <200angel@live.com.mx>
2023-03-15 22:14:33 -0600

    Adding header, input and button to call api. Call api is on building process

--- gif-expert-app/src/GifExpertApp.tsx
+++ gif-expert-app/src/GifExpertApp.tsx
@@ -1,17 +1,37 @@
-import React, {ReactElement, useState} from "react";
+import React, {KeyboardEventHandler, ReactElement, useState} from "react";
 
 export const GifExpertApp = () : ReactElement => {
 
-    const [categories, setCategories] = useState(['One Punch','Saitama'])
+    const [categories, setCategories] = useState(['One Punch','Saitama']);
+    const [category, setCategory] = useState('');
+
+    const onAddCategory = () => {
+        setCategories((prevState) => [...prevState, category]);
+        setCategory('');
+    }
+    const handleDataEnter = (event: any) => {
+        event.preventDefault();
+        setCategory(`${event.target.value}`);
+    }
 
     return (
       <div className="container">
-        <h1>Gif Expert App</h1>
-       <ol>
+        <hgroup>
+          <h1>Gif Expert App</h1>
+          <h2>Using React JS and PicoCSS</h2>
+        </hgroup>
+        <form>
+          <label htmlFor="category">Search category</label>
+          <input type="text" id="category" name="category" placeholder="Category" required onChange={handleDataEnter} value={category}/>
+        </form>
+        <button type={"button"} onClick={onAddCategory}>Add Category</button>
+        <ol>
            {
-               categories.map((categorie:string, id:number) => <li key={id}>{categorie}</li>)
+               categories.map((category:string, id:number) => {
+                   return <li key={id}>{ category }</li>;
+               })
            }
-       </ol>
+        </ol>
       </div>
     );
 };
